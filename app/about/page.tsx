f1a9a8186bde34173b165badab38fd28ccac847b
Isaac Sutton-Wild <isaacsw999@gmail.com>
2024-02-25 15:05:27 +0000

    feat: add about page and update menu icon

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function about() {
+    return (
+        <main className={styles.main}>
+            <p>This is the about page</p>
+        </main>
+    );
+}
