d96583730223aaf5ad4a45963d1bd1fe7a90da06
Isaac Sutton-Wild <isaacsw999@gmail.com>
2024-05-21 14:11:41 +0100

    style: Update padding values and image styling in welcome page.

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import StartWritingButton from "../components/button/button";
+import StartWritingButton from "../../components/button/button";
 
 export default function Welcome() {
   return (
     <div className={styles.container}>
-      <Image
-        src="/welcomeSplash.png"
-        alt="Welcome Icon"
-        width={800}
-        height={800}
-      />
+      <div className={styles.imageWrapper}>
+      </div>
       <div className={styles.wrapper}>
         <p>
           Welcome to the writers nook I hope you enjoy the vibes and write
